cac3fcf2f1f7edae6b1032553043cce3303b0e88
Sunbearian <stigcollins@icloud.com>
2022-12-09 12:00:25 +0000

    Completed PATCH and DELETE

--- models/todos.js
+++ models/todos.js
@@ -1,18 +1,36 @@
 import { pool } from "../db/index.js";
 
 export async function getTodos() {
-    const data = await pool.query("SELECT * FROM todos ORDER BY id;");
-    return data.rows;
+	const data = await pool.query("SELECT * FROM todos ORDER BY id;");
+	return data.rows;
 }
 
 export async function postTodo(newTodo) {
-    const { task, completed } = newTodo;
-    const data = await pool.query(
-        `INSERT INTO todos (
+	const { task, completed } = newTodo;
+	const data = await pool.query(
+		`INSERT INTO todos (
             task,
             completed
         ) VALUES ($1,$2) RETURNING *;`,
-            [task, completed]
-    );
-    return data.rows[0];
+		[task, completed]
+	);
+	return data.rows[0];
+}
+
+//take an id, update the completed field
+
+export async function updateCompleted(id, completedStatus) {
+	const { completed } = completedStatus;
+	const data = await pool.query(
+		`UPDATE todos SET completed = $1 WHERE id = $2 RETURNING *;`,
+		[completed, id]
+	);
+	return data.rows[0];
+}
+
+export async function deleteTodo(id) {
+	const data = await pool.query(`DELETE FROM todos WHERE id = $1 RETURNING *`, [
+		id,
+	]);
+	return data.rows[0];
 }
--- routes/todos.js
+++ routes/todos.js
@@ -2,22 +2,36 @@ import express, { Router } from "express";
 const todosRouter = express.Router();
 
 import {
-    getTodos,
-    postTodo
-} from "../models/todos.js"
+	getTodos,
+	postTodo,
+	updateCompleted,
+	deleteTodo,
+} from "../models/todos.js";
 
 // Get the todos list
 todosRouter.get("/", async (req, res) => {
-    const data = await getTodos();
-    res.status(200).json({success:true, payload:data});
-})
+	const data = await getTodos();
+	res.status(200).json({ success: true, payload: data });
+});
 
 // Post a todo
 todosRouter.post("/", async (req, res) => {
-    const newTodo = req.body;
-    const result = await postTodo(newTodo);
-    res.status(201).json({success:true, payload:result});
-})
+	const newTodo = req.body;
+	const result = await postTodo(newTodo);
+	res.status(201).json({ success: true, payload: result });
+});
 
+todosRouter.patch("/:id", async (req, res) => {
+	const id = req.params.id;
+	const updatedTodo = req.body;
+	const result = await updateCompleted(id, updatedTodo);
+	res.status(200).json({ success: true, payload: result });
+});
+
+todosRouter.delete("/:id", async (req, res) => {
+	const id = req.params.id;
+	const result = await deleteTodo(id);
+	res.status(200).json({ success: true, payload: result });
+});
 
 export default todosRouter;
