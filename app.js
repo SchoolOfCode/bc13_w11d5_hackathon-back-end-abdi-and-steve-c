031dcde01d998cde5f5b1507171c0f4100227904
Sunbearian <stigcollins@icloud.com>
2022-12-09 09:48:57 +0000

    Created the folder structure and files

---
+++ app.js
@@ -0,0 +1,13 @@
+//import express
+// import cors
+// import morgan
+// import todosRouter from routes/todos.js
+
+//create app from express
+
+//cors
+//morgan
+//express json
+//app. use
+
+//export default app
