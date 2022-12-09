439c43f201a6233eb20f37060504b4313c2e5d09
Abdiqani Mohamed <Mohamedabdiqani321@gmail.com>
2022-12-09 11:43:43 +0000

    added post

--- models/todos.js
+++ models/todos.js
@@ -4,3 +4,15 @@ export async function getTodos() {
     const data = await pool.query("SELECT * FROM todos ORDER BY id;");
     return data.rows;
 }
+
+export async function postTodo(newTodo) {
+    const { task, completed } = newTodo;
+    const data = await pool.query(
+        `INSERT INTO todos (
+            task,
+            completed
+        ) VALUES ($1,$2) RETURNING *;`,
+            [task, completed]
+    );
+    return data.rows[0];
+}
--- routes/todos.js
+++ routes/todos.js
@@ -2,7 +2,8 @@ import express, { Router } from "express";
 const todosRouter = express.Router();
 
 import {
-    getTodos
+    getTodos,
+    postTodo
 } from "../models/todos.js"
 
 // Get the todos list
@@ -10,5 +11,13 @@ todosRouter.get("/", async (req, res) => {
     const data = await getTodos();
     res.status(200).json({success:true, payload:data});
 })
+
+// Post a todo
+todosRouter.post("/", async (req, res) => {
+    const newTodo = req.body;
+    const result = await postTodo(newTodo);
+    res.status(201).json({success:true, payload:result});
+})
+
 
 export default todosRouter;
